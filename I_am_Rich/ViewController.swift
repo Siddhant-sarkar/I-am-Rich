41b6ac533187329bd5b5d6f531c01bac0f940466
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-10-03 09:01:06 +0530

    Initial Commit

---
+++ I_am_Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I_am_Rich
+//
+//  Created by Siddhant Sarkar on 03/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
